05e0f963760c9e4a4e93aefa9ace3173c331f946
jara <jaramangukiya@gmail.com>
2023-05-29 14:47:48 +0530

    signup page completed

---
+++ script/signup.js
@@ -0,0 +1,12 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    let user={
+        name:document.getElementById("name").value,
+        number:document.getElementById("number").value,
+        email:document.getElementById("email").value,
+        password:document.getElementById("password").value
+    }
+    localStorage.setItem("user",JSON.stringify(user));
+    console.log(user);
+})
