e488d2246e9d101412a347a20a2cd949cbe98b60
jara <jaramangukiya@gmail.com>
2023-06-01 14:56:06 +0530

    product page completed

--- script/product.js
+++ script/product.js
@@ -5,6 +5,37 @@ document.getElementById("nav").innerHTML=nav()
 let data=async()=>{
 let res=await fetch("https://dummyjson.com/products")
 let data=await res.json()
-console.log(data.products);
+display(data.products)
 }
 data()
+
+const display=(data)=>{
+    console.log(data);
+data.map((ele)=>{
+    
+    let img=document.createElement("img")
+    img.src=ele.images[1]
+    let title=document.createElement("p")
+    title.innerHTML=ele.title
+    let price=document.createElement("p")
+    price.innerHTML=ele.price
+    let brand=document.createElement("button")
+    brand.innerHTML=ele.brand
+    let category=document.createElement("button")
+    category.innerHTML=ele.category
+    let rating=document.createElement("p")  
+    rating.innerHTML=ele.rating
+    let btn1=document.createElement("button")
+    btn1.innerHTML="Add to cart"
+let btn2=document.createElement("button")
+btn2.innerHTML="Buy now"
+btn1.style.backgroundColor="green"
+    let div=document.createElement("div")
+    div.append(img, title, price, brand,category, rating, btn1, btn2)
+    document.querySelector(".box2").append(div)
+
+
+})
+}
+
+
